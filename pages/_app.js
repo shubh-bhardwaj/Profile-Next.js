46bc940e01a9bf6917005803f54ff12fee419186
shubh-bhardwaj <you@example.com>
2021-01-21 16:33:04 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded by Creative Ti
 
 =========================================================
 
